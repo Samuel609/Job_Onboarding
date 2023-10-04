984a86c1b0758b6abc9aa63a01e6f6d4ae63cb02
Samuel Abera <samabera03@gmail.com>
2023-10-04 11:24:13 +0300

    validation on job, contact information and application with factory setup

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,2 +1,25 @@
 class Job < ApplicationRecord
+    AVAILABLE = "Available".freeze 
+    NOT_AVAILABLE = "Not_Available".freeze 
+    AVAILABILITY = [Available, NOT_AVAILABLE].freeze
+
+    REMOTE_TEMPORARY = "Remote_Temporary".freeze
+    REMOTE_PERMANENT = "Remote_Permanent".freeze
+    ONSIGHT_TEMPORARY = "Onsight_Temporary".freeze
+    ONSIGHT_PERMANENT = "Onsight_Permanent".freeze
+    TYPE = [REMOTE_PERMANENT, REMOTE_TEMPORARY, ONSIGHT_PERMANENT, ONSIGHT_TEMPORARY].freeze
+
+    MEDICINE = "Medicine".freeze
+    SOCIAL_WORK = "Social_Work".freeze
+    STEM = "Stem".freeze
+    CATEGORY = [MEDICINE, SOCIAL_WORK, STEM].freeze
+
+    belongs_to :contact_information
+    validates :lowest_salary, :highest_salary, numericality: { greater_than: 0 }
+    validates :title, :category, :availability, :type, :description, :cv, :lowest_salary, :highest_salary, :criteria, :logo, :contact_informations, presence: true
+    validates :availability, inclusion: {in: AVAILABILITY}
+    validates :type, inclusion: {in: TYPE}
+    validates :category, inclusion: {in: CATEGORY}
+
+
 end
--- spec/factories/jobs.rb
+++ spec/factories/jobs.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :job do
-    
+    title { Faker::Lorem.word }
+    category { JOb::Stem }
+    availability { Job::Available }
+    type { Job::Onsight_Permanent }
+    description { Faker::Lorem.sentence }
+    cv { Faker::Lorem.sentence }
+    lowest_salary { 1 }
+    highest_salary { 2 }
+    criteria { Faker::Lorem.sentence }
+    logo { Faker::Lorem.sentence }
+    contact_information
+
+
   end
 end
